d7f25df5baebc41ebd05858158fa257c28e518e5
rseck <esquirar@gmail.com>
2024-10-07 15:52:29 +0300

    salva varitate skeleton work

---
+++ src/generate_salva_varitate.py
@@ -0,0 +1,11 @@
+
+def main():
+    #open excel
+    #convert excel to json
+    # save each suting premise and hypothesis together and main verb, save to resources folder
+    # manually create json for formats of entailement and entail/contradiction/neutral
+    # generate all the prompts
+    pass
+
+if __name__ == '__main__':
+    main()
